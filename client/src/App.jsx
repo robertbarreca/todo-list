95ab7ec0afb0b976a0da79af5fdac8bbde2863e1
Robert Barreca <barreca@wisc.edu>
2024-09-15 10:54:15 -0500

    connect backend w frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch("/api/todos")
+      const todos = await res.json()
+      setMessage(todos.msg)
+    }
+    getTodos()
+  }, [])
+
   return (
     <main className="container">
       <h1>Todo List</h1>
+      {message && <p>{message}</p>}
     </main>
   );
 }
